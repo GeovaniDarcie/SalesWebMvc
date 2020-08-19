7209e5e23dade765e8b0a94ddf5fb6a7c977385f
geovaniDarcie <geovani_darcie@hotmail.com>
2020-08-19 10:27:27 -0300

    other entities and second migration

--- Data/SalesWebMvcContext.cs
+++ Data/SalesWebMvcContext.cs
@@ -11,5 +11,7 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<Department> Department {get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Peding = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
